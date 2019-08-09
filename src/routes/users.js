94d99ef3446f2d644992e68d79c2046e12a0b789
PSJ <astro.psj@gmail.com>
2019-08-10 02:36:26 +0900

    Merge

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,24 +1,36 @@
 import express from 'express';
+import User from '../models/User';
 
 const router = express.Router();
 
-
-// create users list
-router.post('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get('/:id/trust', (req, res) => {
+  const { id } = req.body;
+  User.findOne({ id })
+    .then(user => res.json(user.trusted_ip));
 });
 
-// read user by user id
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.post('/:id/trust', (req, res) => {
+  const { id } = req.body;
+  const { ip } = req.body;
+  const regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+  if (regex.test(ip)) {
+    User.updateOne({ id }, { $push: { trusted_ip: ip } })
+      .then(() => res.status(200).end());
+  } else {
+    res.status(401).end();
+  }
 });
 
-// read users list
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.delete('/:id/trust', (req, res) => {
+  const { id } = req.body;
+  const { ip } = req.body;
+  const regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+  if (regex.test(ip)) {
+    User.updateOne({ id }, { $pull: { trusted_ip: ip } })
+      .then(() => res.status(200).end());
+  } else {
+    res.status(401).end();
+  }
 });
 
-// delete users list
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+export default router;
