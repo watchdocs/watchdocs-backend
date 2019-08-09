efd270adcef8bb6e32755402834468238115fdac
kang-heewon <itphantom@icloud.com>
2019-08-10 03:17:06 +0900

    modify router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
 import documentRouter from './document';
+import auth from './userAuth';
+import search from './search';
+import users from './users';
 
 const router = express.Router();
 router.use('/document', documentRouter);
+
+router.use('/auth', auth);
+router.use('/search', search);
+router.use('/users', users);
+
+export default router;
